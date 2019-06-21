a27a42a4b9b7d551af9d7cbdfd1cdb5252104fb7
Alexander Demin <aleksandr.demin@ingate.ru>
2019-06-21 13:45:20 +0300

    Docker fix

--- front/src/api/api.js
+++ front/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-  baseURL: `http://client.ingate.ru/Offers/FilterRivals/api/`,
+  baseURL: `api/`,
   auth: {
     username: 'offeruser',
     password: 'D_f$7u-SVX9v"h;j'
