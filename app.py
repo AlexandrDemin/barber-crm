c9bbf5c4b504b25cb62afe81b1ffe514fd31b999
Aleksandr Demin <noidea91@gmail.com>
2019-09-15 15:45:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#! /home/devel/anaconda2/envs/py35/bin/python3.6
+#! python3
 # -*- coding: utf-8 -*-
 
 from flask import Flask, abort, url_for, render_template, request, Response, current_app, send_from_directory
