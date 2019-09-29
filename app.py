e19bdf48e6a8b35716e4ed4e8413b07a72d0bf9c
mimaos <54391324+mimaos@users.noreply.github.com>
2019-09-29 17:16:46 +0300

    Update app.py
    Добавил методы для всех гетов.
    Добавил метод Upsert. На вход структура такая:
    {
    "table":"название таблицы, в которую нужно сделать апсерт",
    "params":[{ }] либо словарь параметров для апсерта, если одна запись, либо список словарей, если записей много
    }

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def check_auth(username, password):
 
 def authenticate():
     return Response(
-    'Некорректный логин и пароль. Пожалуйста, перезапустите браузер, и попробуйте ещё раз.', 401,
+    'Некорректный логин и пароль. Пожалуйста, перезапустите браузер и попробуйте ещё раз.', 401,
     {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
 def requires_auth(f):
@@ -40,24 +40,6 @@ def writeLog(logEntry):
         json.dump(logEntry, logFile, ensure_ascii=False)
         logFile.write('\n')
         
-def connect(host,database,user,password,query):
-    conn = psycopg2.connect(host=host,database=database, user=user, password=query)
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-    try:
-        cur.execute(query)
-        result = cur.fetchall()
-        cur.close()
-        conn.close()
-
-        if len(result_data) == 0:
-            return {'error':'Нет таких данных'}
-        else:
-            result = result_data[0]
-            return result
-
-    except Exception as e:
-        return [{'Error':e}]
-
 @app.route("/api/Test/", methods=['POST', 'GET'])
 @requires_auth
 def get_projects():
@@ -80,144 +62,117 @@ def get_projects():
 def GetUserData():
     if request.method == 'POST':
         data = request.get_json()
-        login = data['login']
-        query = f"""select name,"pictureUrl",roles from _user where login = '{login}'"""                
-        result = connect("localhost","barbers","read_only","User_ro",query)
+        configkey = 'GetUserData'
+        result = get(configkey,data)
         return result
 
-#добавить операции
 @app.route('/api/GetCurrentSession/', methods=['POST'])
 def GetCurrentSession():
     if request.method == 'POST':
         data = request.get_json()
-        officeid = data['id']        
-        query = f"""select * from session where state = 'open' and "officeId" = {officeid}"""               
-        result_data = connect("localhost","barbers","read_only","User_ro",query)
-        
-        if len(result_data) == 0:
-            return {'error':'Нет таких данных'}
-        else:
-            return result_data[0]
+        configkey = 'GetCurrentSession'
+        result = get(configkey,data)
+        return result
 
 @app.route('/api/GetSession/', methods=['POST'])
 def GetSession():
     if request.method == 'POST':
         data = request.get_json()
-        sessionid = data['id']
-        
-        query_session = f"""select * from session where id = {sessionid}"""
-        
-        result_session = connect("localhost","barbers","read_only","User_ro",query_session)
-        result_session = dict(result_session[0])
-        if 'withOperations' in data:
-            pass
-        elif data['withOperations'] == 'true':
-            print('Добавляем операции')
-            query_serviceoperation = f"""select * from serviceoperation where "sessionId" = {sessionid}"""
-            result_serviceoperation = connect("localhost","barbers","read_only","User_ro", query_serviceoperation)
-
-            query_goodsoperation = f"""select * from goodsoperation where "sessionId" = {sessionid}"""
-            result_goodsoperation = connect("localhost","barbers","read_only","User_ro", query_goodsoperation)
-
-            query_employeepayment = f"""select * from employeepayment where "sessionId" = {sessionid}"""
-            result_employeepayment = connect("localhost","barbers","read_only","User_ro", query_employeepayment)
-
-            query_spendoperations = f"""select * from spendoperations where "sessionId" = {sessionid}"""
-            result_spendoperations = connect("localhost","barbers","read_only","User_ro", query_spendoperations)
-
-            operations = [result_serviceoperation,result_goodsoperation,result_employeepayment,result_spendoperations]
-            result_session['Operations'] = operations
-        return result_session
+        configkey = 'GetSession'
+        result = get(configkey,data)
+        return result
 
 @app.route('/api/GetOffices/', methods=['POST'])
 def GetOffices():
     if request.method == 'POST':
-        query = f"""select * from office"""
-        result = connect("localhost","barbers","read_only","User_ro",query)
-        result = json.dumps([*map(dict, result)])       
-        return result    
+        configkey = 'GetOffices'
+        result = get(configkey)
+        return result
 
 @app.route('/api/GetAdmins/', methods=['POST'])
 def GetAdmins():
     if request.method == 'POST':
-        query = f"""select * from _user where 'officeAdmin' = ANY (roles)
-        order by state desc, name asc"""
-        result = connect("localhost","barbers","read_only","User_ro",query)
-        result = json.dumps([*map(dict, result)])
-        return result 
+        configkey = 'GetAdmins'
+        result = get(configkey)
+        return result
 
 @app.route('/api/GetMasters/', methods=['POST'])
 def GetMasters():
     if request.method == 'POST':
-        query = f"""select * from _user where 'master' = ANY (roles)
-        order by state desc, name asc"""
-        result = connect("localhost","barbers","read_only","User_ro",query)
-        result = json.dumps([*map(dict, result)])
+        configkey = 'GetMasters'
+        result = get(configkey)
         return result
-
+    
 @app.route('/api/GetServicesPrices/', methods=['POST'])
 def GetServicesPrices():
     if request.method == 'POST':
-        query = f"""select * from service"""
-        result = connect("localhost","barbers","read_only","User_ro",query)
-        result = json.dumps([*map(dict, result)])
-        return result
+        configkey = 'GetServicesPrices'
+        result = get(configkey)
+        return result    
 
 @app.route('/api/GetServiceOperation/', methods=['POST'])
 def GetServiceOperation():
     if request.method == 'POST':
         data = request.get_json()
+        configkey = 'GetServiceOperation'
+        result = get(configkey,data)
+        return result
 
-        operationid = data['id']
-        query = f"""select * from serviceoperation where id = '{operationid}'"""
-        result = connect("localhost","barbers","read_only","User_ro",query)
-
-        return dict(result[0])
-
-    
 @app.route('/api/GetGoodsOperation/', methods=['POST'])
 def GetGoodsOperation():
     if request.method == 'POST':
         data = request.get_json()
+        configkey = 'GetGoodsOperation'
+        result = get(configkey,data)
+        return result
 
-        operationid = data['id']
-        query = f"""select * from goodsoperation where id = '{operationid}'"""
-        result = connect("localhost","barbers","read_only","User_ro",query)
-
-        return dict(result[0])
-    
 @app.route('/api/GetSpendOperation/', methods=['POST'])
 def GetSpendOperation():
     if request.method == 'POST':
         data = request.get_json()
-
-        operationid = data['id']
-        query = f"""select * from spendoperation where id = '{operationid}'"""
-        result = connect("localhost","barbers","read_only","User_ro",query)
-
-        return dict(result[0])
+        configkey = 'GetSpendOperation'
+        result = get(configkey,data)
+        return result
 
 @app.route('/api/GetEmployeePaymentOperation/', methods=['POST'])
 def GetEmployeePaymentOperation():
     if request.method == 'POST':
         data = request.get_json()
-
-        operationid = data['id']
-        query = f"""select * from employeepayment where id = '{operationid}'"""
-        result = connect("localhost","barbers","read_only","User_ro",query)
-
-        return dict(result[0])
+        configkey = 'GetEmployeePaymentOperation'
+        result = get(configkey,data)
+        return result
 
 @app.route('/api/GetClient/', methods=['POST'])
 def GetClient():
     if request.method == 'POST':
         data = request.get_json()
+        configkey = 'GetClient'
+        result = get(configkey,data)
+        return result
 
-        operationid = data['id']
-        query = f"""select * from client where id = '{operationid}'"""
-        result = connect("localhost","barbers","read_only","User_ro",query)
+@app.route('/api/GetEmployeePaymentTypes/', methods=['POST'])
+def GetEmployeePaymentTypes():
+    if request.method == 'POST':
+        configkey = 'GetEmployeePaymentTypes'
+        result = get(configkey)
+        return result
 
-        return dict(result[0])
+@app.route('/api/GetClients/', methods=['POST'])
+def GetClients():
+    if request.method == 'POST':
+        data = request.get_json()
+        configkey = 'GetClients'
+        result = get(configkey,data)
+        return result
+    
+@app.route('/api/Upsert/', methods=['POST'])
+def Upsert():
+    if request.method == 'POST':
+        data = request.get_json()
+        table = data['table']
+        params = data['params']
+        result = edit(table,params)
+        return result
 
 @app.route('/static/<path:path>')
 @requires_auth
