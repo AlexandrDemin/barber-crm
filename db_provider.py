c9fb7a39732b45fb5b3abd84dc107a229e846d57
mimaos <54391324+mimaos@users.noreply.github.com>
2019-09-30 23:02:54 +0300

    Update db_provider.py
    Добавил механику для формирования списка сессий с операциями.
    Точно работает на GetSessionsWithOperations по любым параметрам, но получение текущей сессии и сессии по ID не проверял.

--- db_provider.py
+++ db_provider.py
@@ -6,6 +6,7 @@ import json
 import psycopg2
 
 #DB
+#DB
 def prepareData(data):
     for key,value in data.items():
         if type(value) == dict:
@@ -69,8 +70,82 @@ def generateWhere(data):
     whereclause = baseq + wherestring
     return whereclause
 
+def getSessionsQuery(args):
+    table = args['table']
+    fields = ','.join(args['fields'])
+    qdatefrompart = ''
+    qdatetopart = ''
+    wherepartlist = []
+
+    if 'dateFrom' in args['data']:
+        date = args['data']['dateFrom']
+        qdatefrompart = f"""\"dateOpened\" >= '{date}'""" 
+
+    if 'dateTo' in args['data']:
+        date = args['data']['dateTo']
+        qdatetopart = f"""\"dateClosed\" <= '{date}'"""
+
+    if 'employeeIds' in args['data']:
+        fields_filtered = args['fields'].copy()
+        fields_filtered.remove('"employees"')
+        fields = ','.join(fields_filtered)
+        employeeids = args['data']['employeeIds']
+        employeeidslistformatted = [* map(str, employeeids)]
+        employeelisttoquery = '\''+'\',\''.join(employeeidslistformatted)+ '\''
+        employeeidspart = f"""s.employeesunnsted->>'userId' in ({employeelisttoquery})"""
+        wherelist = [employeeidspart,qdatefrompart,qdatetopart]
+        wherelistfiltered = list(filter(None, wherelist))
+        wherelocalpart = ' and '.join(wherelistfiltered)
+        sessionquery = f"""select * from
+        (select {fields},array_agg(employeesunnsted) as employees from
+        (SELECT *,unnest(employees) employeesunnsted FROM {table}) s
+        where {wherelocalpart}
+        group by {fields}) ssn
+        """
+    else:
+        wherelist = [qdatefrompart,qdatetopart]
+        wherelistfiltered = list(filter(None, wherelist))
+        wherelocalpart = ' and '.join(wherelistfiltered)
+        sessionquery = f"""select * from
+        (select {fields} from {table}) ssn
+        where {wherelocalpart}"""
+
+    if 'withOperations' in args['data']:
+        args['data']['operationType'] = [1,2,3,4]
+
+    if 'operationType' in args['data']:
+        # фильтрация по id сессий
+        operationtypes = {1:"serviceoperation",2:"goodsoperation",3:"spendoperation",4:"employeepayment"}
+        querypartslist = []
+        for item in args['data']['operationType']:
+            table = operationtypes[item]
+            if operationtypes[item] in ['serviceoperation','goodsoperation']:
+                if 'clientIds' in args['data']:
+                    clientids = args['data']['clientIds']
+                    clientidslistformatted = [* map(str, clientids)]
+                    clientidslisttoquery = '('+','.join(clientidslistformatted) + ')'
+                    clientidspart = f"""
+                    where \"clientId\" in {clientidslisttoquery}"""
+                else:
+                    clientidspart = ''
+            qpart = f"""select "sessionId",'{table}' as type,row_to_json({table}) as params from {table}{clientidspart}"""
+            querypartslist.append(qpart)
+        unions = "\nunion all\n".join(querypartslist)
+        queryoperations = f'''select c."sessionId",array_agg(row_to_json(c)::jsonb-'sessionId') as "Operations" from
+        (select concatenated."sessionId",concatenated.type,array_agg(params) as operations from
+        ({unions}) concatenated
+        group by "sessionId",type) c
+        group by "sessionId"'''
+
+    query = f'''{sessionquery}
+    inner join
+    ({queryoperations}) operations
+    on ssn.id = operations."sessionId"'''
+    return query
+
 def generateQueryRead(args=None):
     table = args['table']
+    additionalpart = ''
     orderpart = ''
     wherepart = ''
     fields = '*'
@@ -79,32 +154,43 @@ def generateQueryRead(args=None):
         fields = ','.join(args['fields'])
     
     if 'type' in args:
-        if args['type'] == 'GetAdmins':
-            role = rolesdict[args['type']]
-            wherepart = f"""
-            where 'officeAdmin' = ANY (roles)"""
-            orderpart = f"""
-            order by state desc, name asc"""
+        if args['type'] == 'GetSessions':
+            query = getSessionsQuery(args)
+            
+        else:
+            if args['type'] in ['GetAdmins','GetMasters']:
+                if args['type'] == 'GetMasters':
+                    fields = '_user.' + ',_user.'.join(args['fields']) + ',b."servicePercent" as "masterServicePercent"'
+                    additionalpart = f"""
+                    left join
+                    (select id,"servicePercent" from barbercategory) b
+                    on _user."categoryId"=b.id"""
+
+                rolesdict = {'GetAdmins':'officeAdmin','GetMasters':'master'}
+                role = rolesdict[args['type']]
+                wherepart = f"""
+                where '{role}' = ANY (roles)"""
+                orderpart = f"""
+                order by state desc, name asc"""
+
+            elif args['type'] == 'GetClients':
+                string = args['data']['q'].lower()
+                wherepart = f"""
+                where name ilike '%{string}%'
+                or  text(contacts) ilike '%{string}%'"""
+                orderpart = f"""
+                order by name asc"""
+                
+            if 'data' in args:
+                wherepart = generateWhere(args['data'])
+                
+            query = f"""select {fields} from {table}{additionalpart}{wherepart}{orderpart}"""
         
-        if args['type'] == 'GetMasters':
-            wherepart = f"""
-            where 'master' = ANY (roles)"""
-            orderpart = f"""
-            order by state desc, name asc"""
-            
-        elif args['type'] == 'GetClients':
-            string = args['data']['q'].lower()
-            wherepart = f"""
-            where name ilike '%{string}%'
-            or  text(contacts) ilike '%{string}%'"""
-            orderpart = f"""
-            order by name asc"""
-            
     else:    
         if 'data' in args:
             wherepart = generateWhere(args['data'])            
             
-    query = f"""select {fields} from {table}{wherepart}{orderpart}"""
+        query = f"""select {fields} from {table}{additionalpart}{wherepart}{orderpart}"""
     return query
 
 def goToBase(host,database,user,password,query,commit=False):
