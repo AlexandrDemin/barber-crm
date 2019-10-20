34c3f7ad4fc38d7542dd3da18300075dd09b6697
alexandr.demin <hseoanalytics@gmail.com>
2019-10-20 23:44:52 +0300

    Some front progress

--- front/src/main.js
+++ front/src/main.js
@@ -4,7 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store/store'
+import moment from 'moment'
 
+Vue.prototype.moment = moment
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
