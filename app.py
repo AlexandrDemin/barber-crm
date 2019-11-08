841084101e4739cea2b96f460645c76ee136e395
alexandr.demin <hseoanalytics@gmail.com>
2019-11-08 14:01:55 +0300

    Some front fixes

--- app.py
+++ app.py
@@ -499,4 +499,4 @@ def page_not_found(e):
     return 'Страница не найдена. <a href="/">На главную</a>', 404
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
